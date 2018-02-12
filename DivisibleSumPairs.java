ed22f5fa72318950d06c91e8940b742318b04993
Tanvir Hasan <30554508+hatanvir@users.noreply.github.com>
2018-02-13 01:10:00 +0600

    With java

---
+++ DivisibleSumPairs.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class DivisibleSumPairs {
+	
+	static int count = 0;
+public static class Solution {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        for(int i=0;i<n;i++){
+        	for(int j=i+1;j<n;j++){
+        		if(i<j){
+        			if((ar[i]+ar[j])%k==0){
+        				count++;
+        			}
+        			else{
+        				continue;
+        			}
+        		}
+        		else{
+        			continue;
+        		}
+        	}
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+    }
+}
+}
